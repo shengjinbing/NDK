1901250021168e78dc6319266ced4d7f5613913e
lixiang <675196450@qq.com>
2020-10-27 09:28:54 +0800

    增加一个超时异常hook

---
+++ timeoutexception2/src/main/java/com/medi/timeoutexception2/SampleApplication.java
@@ -0,0 +1,12 @@
+package com.medi.timeoutexception2;
+
+import android.app.Application;
+import android.content.Context;
+
+public class SampleApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(final Context base) {
+        super.attachBaseContext(base);
+    }
+}
